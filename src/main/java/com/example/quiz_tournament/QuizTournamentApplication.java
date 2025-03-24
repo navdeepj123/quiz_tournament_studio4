27bb33e8d37ae5e53d9389327bae81554c5d2891
navdeepj123 <162513277+navdeepj123@users.noreply.github.com>
2025-03-25 11:42:31 +1300

    Initial commit

---
+++ src/main/java/com/example/quiz_tournament/QuizTournamentApplication.java
@@ -0,0 +1,13 @@
+package com.example.quiz_tournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizTournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizTournamentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quiz_tournament/QuizTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quiz_tournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizTournamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
